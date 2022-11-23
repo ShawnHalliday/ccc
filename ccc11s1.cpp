0f855d26f55c54fa9f1645d9407d6091ddf345f2
ShawnHalliday <114366487+ShawnHalliday@users.noreply.github.com>
2022-11-23 08:43:06 -0800

    ccc11s1

---
+++ ccc11s1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int a[10000];
+int d;
+int e;
+int f;
+string b;
+string c;
+
+int main(){
+    cin >> a[0];
+    for (int i = 0; i < a[0]+1; i++){
+        getline(cin, b);
+        c = c + b;
+    }
+    d = c.length();
+    transform(c.begin(), c.end(), c.begin(), ::toupper);
+    for (int j = 0; j < d; j++){
+        if (c[j] == 'T'){
+            e++;
+        }
+        else if (c[j] == 'S'){
+            f++;
+        }
+    }
+    if (e>f){
+        cout<<"English";
+    }
+    else{
+        cout<<"French";
+    }
+
+    return 0;
+}
